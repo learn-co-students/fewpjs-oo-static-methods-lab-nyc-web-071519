4902df33bc60cf3604dcc64f72225f7953861400
github username <github email address>
2019-09-06 10:42:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
 class Formatter {
   //add static methods here
+
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.substr(1);
+  }
+
+  static sanitize(string) {
+    const stringArray = string.split("");
+    const sanitizedArray = stringArray.map((char) => {
+      if (!(char.match(/^[0-9a-zA-Z]+$/) || char === "-" || char === "'" || char === " ")) {
+        return char.replace(char, "");
+      } else {
+        return char;
+      }
+    });
+    const sanitizedString = sanitizedArray.join("");
+    return sanitizedString;
+  }
+
+  static titleize(string) {
+    const stringArray = string.split(" ");
+    const titleizedArray = stringArray.map((word) => {
+      if (!(word === "the" || word === "a" || word === "an" || word === "but" || word === "of" || word === "and" || word === "for" || word === "at" || word === "by" || word === "from")) {
+        return Formatter.capitalize(word);
+      } else {
+        return word;
+      }
+    });
+    const titleizedString = titleizedArray.join(" ");
+    return Formatter.capitalize(titleizedString);
+  }
+
 }
